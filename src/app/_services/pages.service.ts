5359d496767107df2a2dcdbcebeb6c2294153d69
DIUSFRAMI\egala <egala@diusframi.es>
2024-07-16 13:21:35 +0200

    HTMLS de cierres e informes y cambios CSS

--- src/app/_services/pages.service.ts
+++ src/app/_services/pages.service.ts
@@ -6,10 +6,10 @@ import { Page } from 'src/app/_models/Page';
 })
 export class PagesService {
   public pages : Page[] = [
-    new Page('0','Dashboard','dashboard','fa-solid fa-square-poll-vertical'),
+    new Page('0','Dashboard','dashboard','fa-solid fa-grip'),
     new Page('1','Ventas','sales','fa-solid fa-chart-line'),
     new Page('1','Clientes','clients','fa-solid fa-chart-area'),
-    new Page('1','Cierres  de caja ','balances','fa-regular fa-chart-bar')
+    new Page('1','Cierres  de caja ','balances','fa-solid fa-cash-register')
 ];
 
   constructor() { }
