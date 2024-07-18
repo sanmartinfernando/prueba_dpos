19a94017004b770f075a01c4f99cca7e07d697b1
DIUSFRAMI\egala <egala@diusframi.es>
2024-07-18 09:37:04 +0200

    Estilos

--- src/app/_services/pages.service.ts
+++ src/app/_services/pages.service.ts
@@ -6,10 +6,15 @@ import { Page } from 'src/app/_models/Page';
 })
 export class PagesService {
   public pages : Page[] = [
-    new Page('0','Dashboard','dashboard','fa-solid fa-grip'),
+    new Page('0','Dashboard','dashboard','fi fi-rr-apps'),
+    new Page('1','Ventas','sales','fi fi-rr-chat-arrow-grow'),
+    new Page('1','Informes','clients','fi fi-rr-newspaper'),
+    new Page('1','Cierres  de caja ','balances','fi fi-rs-point-of-sale-bill')
+
+    /*new Page('0','Dashboard','dashboard','fa-solid fa-grip'),
     new Page('1','Ventas','sales','fa-solid fa-chart-line'),
     new Page('1','Clientes','clients','fa-solid fa-chart-area'),
-    new Page('1','Cierres  de caja ','balances','fa-solid fa-cash-register')
+    new Page('1','Cierres  de caja ','balances','fa-solid fa-cash-register')*/
 ];
 
   constructor() { }
