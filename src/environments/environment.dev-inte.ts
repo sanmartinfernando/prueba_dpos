c128b593754f00328ff2bb96388a38f51eb7100c
Junior Ochei <jochei@diusframi.es>
2025-02-24 17:45:13 +0100

    - actualizo la ruta de integracion

--- src/environments/environment.dev-inte.ts
+++ src/environments/environment.dev-inte.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    urlWS:'https://dpos.diusframi.tech:39443/wstickets/',
+    urlWS:'http://dpos-inte-alb-1829415705.eu-central-1.elb.amazonaws.com/wstickets/',
     urlAuth:'https://dpos.diusframi.tech:39443/wsauth/',
     urlWE:'https://dpos.diusframi.tech:39443/wsenrollment/'
 };
