692d3027de13b1b5c136d3a1fc0e003842b7fa57
Junior Ochei <jochei@diusframi.es>
2025-02-27 17:15:50 +0100

    - Se añade primer stage  "BUILD"  al pipeline.

---
+++ src/environments/environment.tpl.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    urlWS:'${HOST_DPOS_CLOUD_TICKETS}', // url DPOS Cloud - Tickets
+    urlAuth:'${HOST_DPOS_CLOUD_AUTH}', // url DPOS Cloud - Auth
+    urlWE:'${HOST_DPOS_CLOUD_ENROLLMENT}' // url DPOS Cloud - Enrollment
+};
