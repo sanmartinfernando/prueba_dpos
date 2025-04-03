235b18a7030d0c498c5f93eeba70619c8e43dc7f
Javier Melero <jmelero@diusframi.es>
2025-04-03 15:34:35 +0200

    implementado un servicio que hace logout de usuario por inactividad con la web

---
+++ src/app/_services/inactivity.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InactivityService {
+  private timeout: any;
+  private readonly INACTIVITY_TIME = 15 * 60 * 1000; // 15 minutos
+  private readonly WARNING_TIME = 10 * 60 * 1000; // Alerta de inactividad después de 14 minutos
+
+  constructor(private router: Router, private ngZone: NgZone, private storageService: StorageService) {}
+
+  startMonitoring() {
+    // Se escucha por eventos de interacción del usuario
+    this.ngZone.runOutsideAngular(() => {
+      window.addEventListener('mousemove', this.resetTimer.bind(this));
+      window.addEventListener('keydown', this.resetTimer.bind(this));
+      window.addEventListener('scroll', this.resetTimer.bind(this));
+      window.addEventListener('click', this.resetTimer.bind(this));
+
+      this.resetTimer(); // Comienza el timer al inicio
+    });
+  }
+
+  resetTimer() {
+    // Cancela cualquier timeout previo
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+
+    // Establece un nuevo timeout para el logout
+    this.timeout = setTimeout(() => {
+      this.logout();
+    }, this.INACTIVITY_TIME);
+
+    // Si está a punto de expirar, podemos mostrar una alerta o realizar otro tipo de acción
+    setTimeout(() => {
+      this.showInactivityWarning();
+    }, this.WARNING_TIME);
+  }
+
+  private showInactivityWarning() {
+    // Aquí podemos mostrar un modal o una alerta para notificar al usuario.
+  }
+
+  private logout() {
+    this.storageService.clean();
+    window.location.reload();
+    this.storageService.updateloggin(false);
+    this.router.navigate(['/login']);
+  }
+
+  stopMonitoring() {
+    // Remueve los event listeners para no seguir monitoreando
+    window.removeEventListener('mousemove', this.resetTimer.bind(this));
+    window.removeEventListener('keydown', this.resetTimer.bind(this));
+    window.removeEventListener('scroll', this.resetTimer.bind(this));
+    window.removeEventListener('click', this.resetTimer.bind(this));
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { InactivityService } from './_services/inactivity.service';
 
 @Component({
   selector: 'DPOSW-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  constructor(private inactivityService: InactivityService) {}
+
+  ngOnInit() {
+    // Comienza a monitorear la inactividad cuando se carga el componente
+    this.inactivityService.startMonitoring();
+  }
+
+  ngOnDestroy() {
+    // Detener el monitoreo si el componente se destruye
+    this.inactivityService.stopMonitoring();
+  }
 }
