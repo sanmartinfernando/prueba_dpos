137bd9c7406efb3a12bfed270f0d81c0ffa6c57f
DIUSFRAMI\dfsanmartin <dfsanmartin@diusframi.es>
2025-10-27 12:08:19 +0100

    fix: environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,13 @@
 //inte: https://apiinte.dpos.es
 //pro: https://api.dpos.es
 export const environment = {
-    production: true,
-    urlClients:'https://q6zmc7oxgg.execute-api.eu-south-2.amazonaws.com/pre/',
-    urlProducts:'https://c9i32wue1f.execute-api.eu-south-2.amazonaws.com/pre/',
-    urlWS:'https://api.dpos.es/wstickets/',
-    urlAuth:'https://dpos.diusframi.tech:39443/wsauth/',
-    urlSettings:'https://3yh5mb6hfj.execute-api.eu-south-2.amazonaws.com/pre',
+    production: false,
+    urlClients: 'https://q6zmc7oxgg.execute-api.eu-south-2.amazonaws.com/pre/',
+    urlProducts: 'https://c9i32wue1f.execute-api.eu-south-2.amazonaws.com/pre/',
+    urlWS: 'https://api.dpos.es/wstickets/',
+    urlAuth: 'https://dpos.diusframi.tech:39443/wsauth/',
+    urlWE: 'https://dpos.diusframi.tech:39443/wsenrollment/',
+    urlSettings: 'https://3yh5mb6hfj.execute-api.eu-south-2.amazonaws.com/pre',
     urlDocuments: 'https://d1v0i5k89q4x6i.cloudfront.net/'
 };
 
